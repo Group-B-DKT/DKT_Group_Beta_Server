516c3daa618238a6dc12bddf9b8d5d395dc7bc6c
chriz133 <melchior133@gmail.com>
2024-04-01 15:48:00 +0200

    connect controller implmented. it checks for already connected player and tries to connect to the server

--- src/main/java/at/aau/serg/dktserver/controller/ConnectController.java
+++ src/main/java/at/aau/serg/dktserver/controller/ConnectController.java
@@ -1,16 +1,26 @@
 package at.aau.serg.dktserver.controller;
 
+import at.aau.serg.dktserver.websocket.handler.WebSocketHandlerImpl;
 import org.springframework.web.socket.WebSocketSession;
 
-import java.net.http.WebSocket;
 import java.util.List;
 
 public class ConnectController {
 
-    public ConnectController(){
+    private WebSocketHandlerImpl webSocket;
 
+    public ConnectController(){
+        webSocket = webSocket.getInstance();
     }
     public void connectUser(String username, String playerId,  int gameId, WebSocketSession session){
+        List<String> playerIds = webSocket.getPlayerIds();
 
+        if (playerIds.contains(playerId)){
+            System.out.println(String.format("Player: %s tries reconnecting to server...", username));
+            webSocket.setSessionOfPlayer(playerId, session);
+            webSocket.reconnectPlayer(playerId);
+            return;
+        }
+        webSocket.connectAndAddPlayer(username, gameId, playerId, session);
     }
 }
