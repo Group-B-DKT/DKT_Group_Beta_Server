d2af24b7c4f83a423cd63882e0760667b4180878
chriz133 <melchior133@gmail.com>
2024-04-01 15:35:55 +0200

    implemented singleton for websocket

--- src/main/java/at/aau/serg/dktserver/websocket/handler/WebSocketHandlerImpl.java
+++ src/main/java/at/aau/serg/dktserver/websocket/handler/WebSocketHandlerImpl.java
@@ -1,9 +1,24 @@
 package at.aau.serg.dktserver.websocket.handler;
 
+import at.aau.serg.dktserver.model.domain.PlayerData;
+import at.aau.serg.dktserver.parser.JsonInputParser;
+import at.aau.serg.dktserver.parser.interfaces.InputParser;
 import org.springframework.web.socket.*;
 
-public class WebSocketHandlerImpl implements WebSocketHandler {
+import java.util.ArrayList;
+import java.util.List;
 
+public class WebSocketHandlerImpl implements WebSocketHandler {
+    private List<PlayerData> playerData;
+
+    private static WebSocketHandlerImpl webSocket;
+    private InputParser inputParser;
+
+    public WebSocketHandlerImpl(){
+        if (webSocket == null) webSocket = this;
+        this.inputParser = new JsonInputParser();
+        this.playerData = new ArrayList<>();
+    }
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
@@ -29,5 +44,11 @@ public class WebSocketHandlerImpl implements WebSocketHandler {
     @Override
     public boolean supportsPartialMessages() {
         return false;
+    }
+
+    public static WebSocketHandlerImpl getInstance(){
+        if (webSocket != null) return webSocket;
+        webSocket = new WebSocketHandlerImpl();
+        return webSocket;
     }
 }
