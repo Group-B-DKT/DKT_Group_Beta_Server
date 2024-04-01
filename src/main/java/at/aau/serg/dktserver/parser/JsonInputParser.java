54be85e70c01f28995d2e8c8cc75ba1d951d7093
chriz133 <melchior133@gmail.com>
2024-04-01 15:23:30 +0200

    Gson library added to project

--- src/main/java/at/aau/serg/dktserver/parser/JsonInputParser.java
+++ src/main/java/at/aau/serg/dktserver/parser/JsonInputParser.java
@@ -2,9 +2,12 @@ package at.aau.serg.dktserver.parser;
 
 import at.aau.serg.dktserver.communication.Wrapper;
 import at.aau.serg.dktserver.parser.interfaces.InputParser;
+import com.google.gson.Gson;
 import org.springframework.web.socket.WebSocketSession;
 
 public class JsonInputParser implements InputParser {
+    private static Gson gson = new Gson();
+
     @Override
     public void parseInput(String client_msg, WebSocketSession session, String fromPlayername) {
 
