54d7685171751321485e86db2fdd8854a9ae11da
chriz133 <melchior133@gmail.com>
2024-04-01 15:41:56 +0200

    added Action parsing and controller

---
+++ src/main/java/at/aau/serg/dktserver/controller/ActionController.java
@@ -0,0 +1,13 @@
+package at.aau.serg.dktserver.controller;
+
+import com.google.gson.Gson;
+import org.example.Gson.communication.ActionJsonObject;
+import org.example.Gson.communication.enums.Action;
+import org.example.Gson.communication.enums.Request;
+import org.example.Gson.communication.utilities.WrapperHelper;
+import org.example.Gson.model.Game;
+import org.example.Gson.websocket.WebSocket;
+
+public class ActionController {
+    // Todo
+}
--- src/main/java/at/aau/serg/dktserver/parser/JsonInputParser.java
+++ src/main/java/at/aau/serg/dktserver/parser/JsonInputParser.java
@@ -4,6 +4,7 @@ import at.aau.serg.dktserver.communication.ConnectJsonObject;
 import at.aau.serg.dktserver.communication.Wrapper;
 import at.aau.serg.dktserver.communication.enums.ConnectType;
 import at.aau.serg.dktserver.communication.utilities.WrapperHelper;
+import at.aau.serg.dktserver.controller.ActionController;
 import at.aau.serg.dktserver.controller.ConnectController;
 import at.aau.serg.dktserver.parser.interfaces.InputParser;
 import com.google.gson.Gson;
@@ -12,6 +13,12 @@ import org.springframework.web.socket.WebSocketSession;
 public class JsonInputParser implements InputParser {
     private static Gson gson = new Gson();
     private ConnectController connectController;
+    private ActionController actionController;
+
+    public JsonInputParser(){
+        actionController = new ActionController();
+        connectController = new ConnectController();
+    }
 
     @Override
     public void parseInput(String client_msg, WebSocketSession session, String fromPlayername) {
@@ -24,6 +31,7 @@ public class JsonInputParser implements InputParser {
         }
         switch (wrapper.getRequest()){
             case CONNECT -> parseConnect(wrapper, session);
+            case ACTION -> parseAction(wrapper, fromPlayername);
         }
     }
 
@@ -36,5 +44,9 @@ public class JsonInputParser implements InputParser {
         switch (connectJsonObject.getConnectType()){
             case NEW_CONNECT -> connectController.connectUser(connectJsonObject.getUsername(), connectJsonObject.getPlayerId(), wrapper.getGameId(), session);
         }
+    }
+
+    private void parseAction(Wrapper wrapper, String fromPLayername){
+        // Todo
     }
 }
