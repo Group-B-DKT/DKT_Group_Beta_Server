b758a525f9551fbce77adc3700dc41aca3f2e829
chriz133 <melchior133@gmail.com>
2024-04-01 15:56:19 +0200

    implemented ActionJsonObject class

--- src/main/java/at/aau/serg/dktserver/communication/ActionJsonObject.java
+++ src/main/java/at/aau/serg/dktserver/communication/ActionJsonObject.java
@@ -1,5 +1,23 @@
 package at.aau.serg.dktserver.communication;
 
+import at.aau.serg.dktserver.communication.enums.Action;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
 public class ActionJsonObject {
-    // Todo
+    private Action action;
+    private String param;
+    private String fromPlayername;
+    
+    public Action getAction() {
+        return action;
+    }
+    public String getParam() {
+        return param;
+    }
+
+    public String getFromPlayername(){return fromPlayername;}
+
 }
