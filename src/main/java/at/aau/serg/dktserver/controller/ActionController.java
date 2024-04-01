fe95a974d125b679780fe08f73c611a9b679504b
chriz133 <melchior133@gmail.com>
2024-04-01 16:00:29 +0200

    implemented the parseAction method to call the Actioncontroller with the correct method

--- src/main/java/at/aau/serg/dktserver/controller/ActionController.java
+++ src/main/java/at/aau/serg/dktserver/controller/ActionController.java
@@ -2,6 +2,7 @@ package at.aau.serg.dktserver.controller;
 
 import at.aau.serg.dktserver.communication.ActionJsonObject;
 import at.aau.serg.dktserver.communication.enums.Action;
+import at.aau.serg.dktserver.communication.enums.Request;
 import at.aau.serg.dktserver.communication.utilities.WrapperHelper;
 import at.aau.serg.dktserver.model.Game;
 import at.aau.serg.dktserver.websocket.handler.WebSocketHandlerImpl;
