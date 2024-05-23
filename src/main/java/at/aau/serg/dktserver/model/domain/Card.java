083ad7500d8cf9d13c143ea31174a854e52f4625
Sylvia Pisjak <sylviapi@edu.aau.at>
2024-05-23 18:43:39 +0200

    implement class card and card types

---
+++ src/main/java/at/aau/serg/dktserver/model/domain/Card.java
@@ -0,0 +1,50 @@
+package at.aau.serg.dktserver.model.domain;
+
+import at.aau.serg.dktserver.model.enums.CardType;
+
+public class Card {
+    private int id;
+    private int amount;
+    private String imageResource;
+    private CardType type;
+
+    public Card(int id, int amount, CardType type,String imageResource) {
+        this.id = id;
+        this.amount = amount;
+        this.imageResource = imageResource;
+        this.type = type;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public void setImageResource(String imageResource) {
+        this.imageResource = imageResource;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public CardType getType() {
+        return type;
+    }
+
+    public void setType(CardType type) {
+        this.type = type;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public String getImageResource() {
+        return imageResource;
+    }
+}
+
