1f4d6bdaa870341e9bc5a269dffee3d0cb1dc341
KevinSappl <sappl.kevke@gmx.at>
2024-06-19 12:11:17 +0200

    Corrected Nullable issue for report cheat method

--- src/main/java/at/aau/serg/dktserver/controller/ActionController.java
+++ src/main/java/at/aau/serg/dktserver/controller/ActionController.java
@@ -300,7 +300,7 @@ public class ActionController {
     private void reportCheat(int gameId, PlayerData fromPlayer, String param) {
         Game game = gameManager.getGameById(gameId);
         PlayerData player = game.getPlayers().stream().filter(playerData -> playerData.getId().equals(param)).findFirst().orElse(null);
-        if(player.isHasCheated()) {
+        if(player != null && player.isHasCheated()) {
             player.setMoney(200);
             player.setHasCheated(false);
             game.goToPrison(player);
